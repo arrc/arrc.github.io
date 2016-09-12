ddcff22592022ca8e173066bbbad3e4aaad4540b
arrC <naveen@arrc.in>
2016-09-12 23:19:03 +0530

    migrated navbar, hero section, vendor, scss files from ui-arrc

---
+++ js/type.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".ar_i_typewritter").typed({
+        strings: [
+            "I'm a self-taught coder cum designer.", 
+            "You know what? ^1000 I love coding.", 
+            "I've watched 'Narcos' more than thrice.",
+            "I mostly listen to tropical house, reggae &amp; tsfh.",
+            "I use linux for development.", 
+            "I use mac for prototyping.", 
+            "I am a web developer.", 
+            "I have a pretty decent web designing skill.", 
+            "I make iPhone applications in swift.",
+            "I've worked on MEAN stack, Rails, iOS &amp; Android.",
+            "I love javascript and swift.",
+        ],
+        typeSpeed: 35,
+        backSpeed: 30,
+        backDelay: 1000,
+        shuffle: true,
+        showCursor: true,
+        cursorChar: "|",
+        loop: true,
+        contentType: 'html',
+    });
+});
