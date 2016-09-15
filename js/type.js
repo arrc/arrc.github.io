440d62aab80fa666de3a8567834101a1b2bdd5fd
arrC <naveen@arrc.in>
2016-09-16 00:32:36 +0530

    updated bio and ican section.

--- js/type.js
+++ js/type.js
@@ -1,17 +1,15 @@
 $(function(){
     $(".ar_i_typewritter").typed({
         strings: [
-            "I'm a self-taught coder cum designer.", 
-            "You know what? ^1000 I love coding.", 
-            "I've watched 'Narcos' more than thrice.",
-            "I mostly listen to tropical house, reggae &amp; tsfh.",
-            "I use linux for development.", 
-            "I use mac for prototyping.", 
-            "I am a web developer.", 
-            "I have a pretty decent web designing skill.", 
+            "I'm a self-taught coder cum designer.",
+            "You know what? ^1000 I love coding.",
+            "I use linux for development.",
+            "I am a web developer.",
+            "I have a pretty decent UI skills.",
             "I make iPhone applications in swift.",
             "I've worked on MEAN stack, Rails, iOS &amp; Android.",
             "I love javascript and swift.",
+            "I'm currently learing React, Elixir, Go &amp; TDD."
         ],
         typeSpeed: 35,
         backSpeed: 30,
